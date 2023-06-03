695adbd64f6f41910a5dda1724f52f31ce6a1f2e
Kamil Saifutdiyarov <ext.ksayfutdiyarov@tinkoff.ru>
2023-06-03 12:30:21 +0300

    V1 Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.larningcourse.learningcourse {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires kotlin.stdlib;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.larningcourse.learningcourse to javafx.fxml;
+    exports com.larningcourse.learningcourse;
+}
