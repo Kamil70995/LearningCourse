1a9fa659d09379622d40479133cdd27f6255997b
Kamil Saifutdiyarov <ext.ksayfutdiyarov@tinkoff.ru>
2023-06-05 21:38:43 +0300

    V5: fix converter

--- src/main/kotlin/com/learningcourse/converter/AuthorDtoToAuthorEntityConverter.kt
+++ src/main/kotlin/com/learningcourse/converter/AuthorDtoToAuthorEntityConverter.kt
@@ -5,5 +5,5 @@ package com.learningcourse.converter
 import com.learningcourse.dto.author.FrontAuthorDto
 import com.learningcourse.entity.AuthorEntity
 
-fun AuthorEntity.toTestFrontDto() =
+fun AuthorEntity.toAuthorFrontDto() =
      FrontAuthorDto(this.name, this.lastName, this.email).also { it.id = this.id }
